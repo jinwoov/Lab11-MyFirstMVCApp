ca1370de5f0d37f3ad08f6820ff75f36efbf264d
Jin Kim <jinwoov@gmail.com>
2020-03-31 22:33:41 -0700

    Designing is all done

--- Lab11-MyFirstMVCApp/Lab11-MyFirstMVCApp/Controllers/HomeController.cs
+++ Lab11-MyFirstMVCApp/Lab11-MyFirstMVCApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Lab11_MyFirstMVCApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,15 +19,19 @@ namespace Lab11_MyFirstMVCApp.Controllers
         [HttpPost]
         public IActionResult Index(int fromYear, int toYear)
         {
-            List<TimePerson> people = TimePerson.GetPersons(fromYear, toYear);
-
-            return RedirectToAction("Results", people);
+            Year year = new Year()
+            {
+                FromYear = fromYear,
+                ToYear = toYear
+            };
+            return RedirectToAction("Results", year);
         }
 
         [HttpGet]
-        public IActionResult Results(List<TimePerson> peeple)
+        public IActionResult Results(Year year)
         {
-            return View(peeple);
+            var people = TimePerson.GetPersons(year.FromYear, year.ToYear);
+            return View(people);
         }
 
     }
---
+++ Lab11-MyFirstMVCApp/Lab11-MyFirstMVCApp/Models/Year.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab11_MyFirstMVCApp.Models
+{
+    public class Year
+    {
+        public int FromYear { get; set; }
+        public int ToYear { get; set; }
+
+    }
+}
